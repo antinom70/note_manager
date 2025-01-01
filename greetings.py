8949f084858039ede7ba48f40513a08152685e0d
Konstantin <costas070@gmail.com>
2025-01-02 02:02:24 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,15 @@
+
+username = 'Сергей'
+print('имя пользователя:', username)
+title = 'Новая книга'
+print('Заголовок: ', title)
+content = 'слова, слова, слова'
+print('Текст заметки ', content)
+status = ('Завершено')
+print('Статус заметки: ', status)
+created_date = '25-10-24'
+print('Дата создания: ', created_date)
+issue_date = '25-12-24'
+print("Дата окончания: ", issue_date)
+
+
